6c807bebb4c5599680fe7575ad3eb62e5400b7e8
unknown <vaibhavkr.singh1994@gmail.com>
2021-05-30 15:56:28 +0530

    feat: add  merge sort base algo

---
+++ index.js
@@ -0,0 +1,15 @@
+const mergeSort = require('./sorting/mergesort/mergesort');
+
+const {
+    startTime,
+    endTime,
+    executionTime,
+    sortedArray 
+} = mergeSort([2,6,9,1,4,8,3,0,5,11,14,15,12,17,71,21,22,88,99,75,34,56,73,17,39,49,52,558,3,0,5,11,14,15,12,17,71,21,22,88,99,75,]);
+
+console.log({
+    startTime,
+    endTime,
+    executionTime,
+    sortedArray
+});
---
+++ sorting/mergesort/mergesort.js
@@ -0,0 +1,43 @@
+const merge = (leftArr = [], rightArr = []) => {
+    const mergedArray = [];
+    let i = 0, j = 0;
+    for (let counter = 0; counter < (leftArr.length + rightArr.length); counter++) {
+        if (j === rightArr.length || leftArr[i] <= rightArr[j]) {
+            mergedArray.push(leftArr[i]);
+            i++;
+        } else {
+            mergedArray.push(rightArr[j]);
+            j++
+        }
+    }
+    return mergedArray;
+}
+
+
+/**
+ * time complexity = O(nlogn)
+ * algo => split - sort & merge
+ */
+const mergeSort = (arr) => {
+    if (arr.length > 1) {
+        const leftArr = mergeSort(arr.slice(0, Math.floor(arr.length/2)));
+        const rightArr = mergeSort(arr.slice(Math.floor(arr.length/2)));
+        return merge(leftArr, rightArr);
+    }
+    return arr;
+};
+
+const initiateSort = (arr) => {
+    const startTime = Date.now();
+    const sortedArray = mergeSort(arr);
+    const endTime = Date.now();
+    return {
+        startTime,
+        endTime,
+        executionTime: endTime-startTime,
+        sortedArray
+    }
+    
+};
+
+module.exports = initiateSort;
