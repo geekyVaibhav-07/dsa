df7be82c14b958762bb9ff78b9d4a44c102824cb
vaibhav <vaibhavkr.singh1994@gmail.com>
2021-06-09 17:48:16 +0530

    feat: add double linked list base algo
    add double linked list algo
    apply optimisation to single linked list

--- list/index.js
+++ list/index.js
@@ -1,4 +1,5 @@
-const SinglyLinkedList = require('./linkedList/linkedlist')
+const SinglyLinkedList = require('./linkedList/linkedlist');
+const DoublyLinkedList = require('./linkedList/doublyLinkedList')
 
 const list = new SinglyLinkedList();
 list.push(10);
@@ -19,3 +20,23 @@ list.reverse();
 console.log(list.toArray());
 console.log(list.toString());
 console.log(list.toString(','));
+
+const DList = new DoublyLinkedList();
+DList.push(10);
+DList.unshift(1);
+DList.push(2);
+DList.push(3);
+DList.shift()
+console.log(DList.toArray());
+DList.insert(1,1)
+console.log(DList.toArray());
+DList.insert(4,4)
+console.log(DList.toArray());
+DList.remove(2)
+console.log(DList.toArray());
+DList.remove(0)
+console.log(DList.toArray());
+DList.reverse();
+console.log(DList.toArray());
+console.log(DList.toString());
+console.log(DList.toString(','));
